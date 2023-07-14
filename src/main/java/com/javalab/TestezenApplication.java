33f6f9041c6718da0f259f6affff039a48bf95bf
SsChany <mashimaro501@naver.com>
2023-07-14 14:13:20 +0900

    firstcommit

---
+++ src/main/java/com/javalab/TestezenApplication.java
@@ -0,0 +1,13 @@
+package com.javalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestezenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestezenApplication.class, args);
+	}
+
+}
